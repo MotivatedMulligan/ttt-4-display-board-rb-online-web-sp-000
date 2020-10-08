7e554924a187cc995a22bbf1c79443aefb24dfff
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-08 22:52:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,8 @@
 
 
 def display_board = display_board(board)
+   display_board = display_board (board)   
+   board = [ “board[0]}” , "#{board[1]}” , "#{board[2]}” , "#{board[3]}” , "#{board[4]}” , "{board[5]}” , "#{board[6]}” , "#{board[7]}” , "#{board[8]}” ]
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts " ----------- "
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
