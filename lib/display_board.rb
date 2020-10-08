38945beb910df478c083fc210e4d487abc4aeef9
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-08 23:08:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,7 +7,7 @@ def display_board(board)
     
    board = [ "" , "" , "" , "" , "" , "" , "" , "" , "" ]
    
-   board = [ “{board[0]} , "{board[1]}” , "{board[2]}” , "{board[3]}” , "#{board[4]}” , "{board[5]}” , "{board[6]}” , "{board[7]}” , "{board[8]}” ]
+   board = [ "{board[1]}" , "{board[1]}" , "{board[2]}" , "{board[3]}" , "{board[4]}" , "{board[5]}" , "{board[6]}" , "{board[7]}" , "{board[8]}" ]
     puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
